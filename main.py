3af5e7fe98df74dd6283721e74a312de014866bd
Khrystia16 <kristinastadnuk2018@gmail.com>
2026-05-24 23:33:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Запускаємо програму...")
+    результат = add_numbers(5, 7)
+    print(f"Результат 5 + 7 = {результат}")
